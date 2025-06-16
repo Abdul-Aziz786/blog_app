a207777315a26f3cb5a1065d22573bf6de8e2fcf
Abdul Aziz <abdulaziz285786@gmail.com>
2025-06-16 17:02:51 +0530

    code changes

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://kfrscawxiuiundhpsjed.supabase.co';
+  static const supabaseUrl = 'https://lurqllkvrtxkemwhvzmn.supabase.co';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmcnNjYXd4aXVpdW5kaHBzamVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5OTA5OTYsImV4cCI6MjAyNTU2Njk5Nn0.PHps6VaSD__4fgz9T14FG2X01VkCYCsbuzUMLtMRCpc';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1cnFsbGt2cnR4a2Vtd2h2em1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NDY1OTQsImV4cCI6MjA2NTQyMjU5NH0.wNrnQ8S3dpyHS34G9VNWKKLRvzaIr3PdvdyGYEvFYu4';
 }
